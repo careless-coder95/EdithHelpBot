657671a3ae041589783a5992fe9f8deefbd248d1
careless-coder95 <dhananjaymaurya3000@gmail.com>
2026-04-04 17:53:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,8 +13,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Required configurations
-API_ID = int(os.getenv("API_ID", 0))
-API_HASH = os.getenv("API_HASH", "")
+API_ID = int(os.getenv("API_ID", 38881334))
+API_HASH = os.getenv("API_HASH", "b28ea0d569fb81f8053484a227514135")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 MONGO_URI = os.getenv("MONGO_URI", "")
 DB_NAME = os.getenv("DB_NAME", "Cluster0")
@@ -24,4 +24,4 @@ BOT_USERNAME = os.getenv("BOT_USERNAME", "EdithHelpsBot")
 
 SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "https://t.me/CarelessxWorld")
 UPDATE_CHANNEL = os.getenv("UPDATE_CHANNEL", "https://t.me/+wQonHdyvZh5mZWM1")
-START_IMAGE = os.getenv("START_IMAGE", "https://files.catbox.moe/n5p1x4.jpg")
+START_IMAGE = os.getenv("START_IMAGE", "https://files.catbox.moe/676gur.jpg")
