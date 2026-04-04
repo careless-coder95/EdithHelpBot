a694fff39cddc1cdeb3e03db89d022967f1810e2
careless-coder95 <dhananjaymaurya3000@gmail.com>
2026-04-04 07:36:57 +0530

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -5,13 +5,11 @@
 
 from .start import register_handlers
 from .group_commands import register_group_commands
-from .repo import register_repo_handler
 from .notes import register_notes_handler
 from .rules import register_rules_handler
 
 def register_all_handlers(app):
     register_handlers(app)
-    register_repo_handler(app)
     register_group_commands(app)
     register_notes_handler(app)
     register_rules_handler(app)
