ba00a01f450026a7ba4c12bd4bfcbe2f8625aa63
careless-coder95 <dhananjaymaurya3000@gmail.com>
2026-04-05 09:10:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,5 @@
 # ============================================================
-#Group Manager Bot
-# Author: LearningBotsOfficial (https://github.com/LearningBotsOfficial) 
-# Support: https://t.me/LearningBotsCommunity
-# Channel: https://t.me/learning_bots
-# YouTube: https://youtube.com/@learning_bots
-# License: Open-source (keep credits, no resale)
+#AUTHOR : MISTER STARK
 # ============================================================
 
 import os
@@ -13,15 +8,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Required configurations
-API_ID = int(os.getenv("API_ID", 38881334))
-API_HASH = os.getenv("API_HASH", "b28ea0d569fb81f8053484a227514135")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-MONGO_URI = os.getenv("MONGO_URI", "")
+API_ID = int(os.getenv("API_ID", 0000000)) #from www.telegram.org
+API_HASH = os.getenv("API_HASH", "YOUR_API_HASH") #from www.telegram.org
+BOT_TOKEN = os.getenv("BOT_TOKEN", "YOU_BOT_TOKEN") #from #Botfather
+MONGO_URI = os.getenv("MONGO_URI", "YOUR_MONGO_URO") # from mongo.db
 DB_NAME = os.getenv("DB_NAME", "Cluster0")
 
-OWNER_ID = int(os.getenv("OWNER_ID", 8275132868))
-BOT_USERNAME = os.getenv("BOT_USERNAME", "EdithHelpsBot")
+OWNER_ID = int(os.getenv("OWNER_ID", 0000000))
+BOT_USERNAME = os.getenv("BOT_USERNAME", "YOUR_BOT_USERNAME") # without @
 
-SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "https://t.me/CarelessxWorld")
-UPDATE_CHANNEL = os.getenv("UPDATE_CHANNEL", "https://t.me/+wQonHdyvZh5mZWM1")
-START_IMAGE = os.getenv("START_IMAGE", "https://files.catbox.moe/676gur.jpg")
+SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "https://t.me/CarelessxWorld") #YOUR SUPPORT GROUP LINK
+UPDATE_CHANNEL = os.getenv("UPDATE_CHANNEL", "https://t.me/+wQonHdyvZh5mZWM1") #YOUR UPDATE CHANELL LINK
+START_IMAGE = os.getenv("START_IMAGE", "https://files.catbox.moe/676gur.jpg") #YOUR START IMAGE LINK GENERATE FROM CATBOX
