fd49ad8ca62e60934fcfbf93a97abec2b92c96c7
careless-coder95 <dhananjaymaurya3000@gmail.com>
2026-04-18 18:02:42 +0530

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -20,9 +20,8 @@ from .cleaner import register_cleaner_handler
 from .blacklist import register_blacklist_handler
 from .filters import register_filters_handler
 from .joinrequest import register_joinrequest_handler
+from .ghost import register_ghost_handler
 
-# andar:
-# andar:
 
 
 def register_all_handlers(app):
@@ -43,4 +42,5 @@ def register_all_handlers(app):
     register_blacklist_handler(app)
     register_filters_handler(app)
     register_joinrequest_handler(app)
+    register_ghost_handler(app)
     print("✅ All handlers registered!")
